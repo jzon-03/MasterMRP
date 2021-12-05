c704cce37953d36f157fedc37f9b1617b9faeb16
Jayson Guevarra <jzon_03@yahoo.com>
2021-12-05 10:11:37 -0500

    Added bootstrap nav component

--- MasterMRP/src/app/app-routing.module.ts
+++ MasterMRP/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './home/account/account.component';
 import { HomeComponent } from './home/home.component';
 import { MailComponent } from './home/mail/mail.component';
+import { SettingsComponent } from './home/settings/settings.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -12,10 +13,11 @@ const routes: Routes = [
     component:HomeComponent,
     children:[
       {path:'account',component:AccountComponent},
-      {path:'mail',component:MailComponent}
+      {path:'mail',component:MailComponent},
+      {path:'settings',component:SettingsComponent}
     ]
   },
-  {path:'login',component:LoginComponent}  
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
