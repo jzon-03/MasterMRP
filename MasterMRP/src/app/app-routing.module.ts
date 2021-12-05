e231d366d247e46f028e7e1eb88f8fe614c1e42b
Jayson Guevarra <jzon_03@yahoo.com>
2021-12-05 09:50:11 -0500

    Added components

--- MasterMRP/src/app/app-routing.module.ts
+++ MasterMRP/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './home/account/account.component';
 import { HomeComponent } from './home/home.component';
+import { MailComponent } from './home/mail/mail.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -9,7 +10,8 @@ const routes: Routes = [
     path:'home',
     component:HomeComponent,
     children:[
-      {path:'account',component:AccountComponent}
+      {path:'account',component:AccountComponent},
+      {path:'mail',component:MailComponent}
     ]
   },
   {path:'login',component:LoginComponent},
--- MasterMRP/src/app/app.module.ts
+++ MasterMRP/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './home/account/account.component';
 import { HomenavComponent } from './home/homenav/homenav.component';
+import { MailComponent } from './home/mail/mail.component';
+import { SettingsComponent } from './home/settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HomenavComponent } from './home/homenav/homenav.component';
     LoginComponent,
     HomeComponent,
     AccountComponent,
-    HomenavComponent
+    HomenavComponent,
+    MailComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
